1194d2902889e2f3989a78300dd07b3483d25ae1
Frank Cline <frankmcline@gmail.com>
2017-03-07 12:56:02 -0900

    Finished parser for CS331

--- CS331/hw4/parseit.lua
+++ CS331/hw4/parseit.lua
@@ -51,41 +51,33 @@ BOOLLIT_VAL = 14
 VARID_VAL   = 15
 ARRAY_REF   = 16
 
--- For lexer iteration
-local iter            -- Iterator returned by lexer.lex
-local state           -- State for above iterator (maybe not used)
-local lexer_out_str   -- Return value #1 from above iterator
-local lexer_out_cat   -- Return value #2 from above iterator
+-- lexer iteration
+local iter
+local state
+local lexer_out_str
+local lexer_out_cat
 
--- For current lexeme
+-- Current lexeme and lexeme category
 local lexstr = ""
 local lexcat = 0
 
-local finalstr = ""
-
--- Utility Functions
-
 -- advance
--- Go to next lexeme and load it into lexstr, lexcat.
--- Should be called once before any parsing is done.
--- Function init must be called before this function is called.
+-- Gets next lexeme and lexeme category from the lexer
+-- and stores it to lexstr and lexcat.
+-- init() must be called before this function is called.
 local function advance()
-    -- Advance the iterator
     lexer_out_str, lexer_out_cat = iter(state, lexer_out_str)
 
-    -- If we're not past the end, copy current lexeme into vars
     if lexer_out_str ~= nil then
         lexstr, lexcat = lexer_out_str, lexer_out_cat
     else
         lexstr, lexcat = "", 0
     end
-
-    finalstr = finalstr.." "..lexstr
 end
 
 
 -- init
--- Initial call. Sets input for parsing functions.
+-- Sets input for parsing functions.
 local function init(prog)
     iter, state, lexer_out_str = lexit.lex(prog)
     advance()
@@ -93,18 +85,20 @@ end
 
 
 -- atEnd
--- Return true if pos has reached end of input.
--- Function init must be called before this function is called.
+-- Return true if the end of the program has been reached
+-- init() must be called before this function is called.
 local function atEnd()
     return lexcat == 0
 end
 
 
 -- matchString
--- Given string, see if current lexeme string form is equal to it. If
--- so, then advance to next lexeme & return true. If not, then do not
--- advance, return false.
--- Function init must be called before this function is called.
+-- Takes a string and compares it to the current lexeme.
+-- If they are equal, advances to the next lexeme and returns true.
+--    If the string is ")", "]", "true", or "false" then an operator is
+--    preferred for the next lexeme.
+-- Otherwise, return false.
+-- init() must be called before this function is called.
 local function matchString(s)
     if lexstr == s then
         if lexstr == ")" or lexstr == "]" or
@@ -120,9 +114,11 @@ end
 
 
 -- matchCat
--- Given lexeme category (integer), see if current lexeme category is
--- equal to it. If so, then advance to next lexeme & return true. If
--- not, then do not advance, return false.
+-- Takes an integer and compares it to the current lexeme category.
+-- If they are equal, advances to the next lexeme and returns true.
+--    If the current lexeme category is a VARID or NUMLIT then an
+--    operator is preferred for the next lexeme.
+-- Otherwise, returns false.
 -- Function init must be called before this function is called.
 local function matchCat(c)
     if lexcat == c then
@@ -136,34 +132,39 @@ local function matchCat(c)
     end
 end
 
+-- parseit.parse
+-- Takes a string that is the program to be parsed.
+-- Returns bool,bool,table
+--    First bool is if the syntax is correct.
+--    Second bool is if the end of program is reached.
+--    Table is the abstract syntax tree of the program.
 function parseit.parse(program)
   init(program)
-
-  -- Get results from parsing
-  local good, ast = parse_stmt_list()  -- Parse start symbol
+  -- program -> statement_list
+  local good, ast = parse_stmt_list()
   local done = atEnd()
 
-  io.write("Parsed: "..finalstr)
-  io.write("\nCategory: " .. lexcat .. "\n")
-  io.write("Lexeme: " .. lexstr .. "\n")
-
-  finalstr = ""
-  -- And return them
   return good, done, ast
 end
 
--- ***********************
+-- *********************************************************
 -- Local Parsing Functions
--- ***********************
+-- 
+-- Named: parse_<nonterminal name>
+--
+-- Returns bool,table
+--    Bool is true if the syntax is correct
+--    Table is the abstract syntax tree of the nonterminal.
+-- *********************************************************
 
 -- parse_stmt_list
+-- stmt_list → { statement }
 function parse_stmt_list()
   local good, ast, newast
 
   ast = { STMT_LIST }
 
   while true do
-
     if lexstr ~= "cr" and lexstr ~= "print" and lexstr ~= "input" and
       lexstr ~= "set" and lexstr ~= "sub" and lexstr ~= "call" and
       lexstr ~= "if" and lexstr ~= "while" then
@@ -182,8 +183,16 @@ function parse_stmt_list()
 end
 
 -- parse_stmt
+-- statement → “cr”
+--           | “print” ( STRLIT | expr )
+--           | “input” lvalue
+--           | “set” lvalue “:” expr
+--           | “sub” SUBID stmt_list “end”
+--           | “call” SUBID
+--           | “if” expr stmt_list { “elseif” expr stmt_list } [ “else” stmt_list ] “end”
+--           | “while” expr stmt_list “end”
 function parse_stmt()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   if matchString("cr") then
     return true, { CR_STMT }
@@ -208,7 +217,7 @@ function parse_stmt()
     ast = { INPUT_STMT, ast }
 
   elseif matchString("set") then
-    good,ast = parse_lvalue
+    good,ast = parse_lvalue()
     if not good or not matchString(":") then
       return false,nil
     end
@@ -219,25 +228,34 @@ function parse_stmt()
     ast = {SET_STMT, ast, newast}
 
   elseif matchString("sub") then
-    oldlexstr = lexstr
-    good,ast = parse_stmt_list()  
-    if not good and not matchString("end") then
+    local subid = lexstr
+    if not matchCat(lexit.SUBID) then
       return false,nil
     end
-    ast = { SUB_STMT, oldlexstr, ast }
+    good,ast = parse_stmt_list()
+    if not good or not matchString("end") then
+      return false,nil
+    end
+    ast = { SUB_STMT, subid, ast }
 
   elseif matchString("call") then
-    ast = { CALL_STMT, lexstr }
+    oldlexstr = lexstr
+    if not matchCat(lexit.SUBID) then
+      return false,nil
+    end
+    ast = { CALL_STMT, oldlexstr }
     return true,ast
 
   elseif matchString("if") then
+    local expr,stmt_list
+
     good,expr = parse_expr()
     if not good then
       return false,nil
     end
 
     good,stmt_list = parse_stmt_list()
-    if not good or not matchString("end") then
+    if not good then
       return false,nil
     end
 
@@ -275,6 +293,8 @@ function parse_stmt()
     end
 
   elseif matchString("while") then
+    local expr,stmt_list
+
     good,expr = parse_expr()
     if not good then 
       return false,nil
@@ -295,9 +315,9 @@ function parse_stmt()
 end
 
 -- parse_expr
--- expr    →   comp_expr { ( “&&” | “||” ) comp_expr }
+-- expr → comp_expr { ( “&&” | “||” ) comp_expr }
 function parse_expr()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   good,ast = parse_comp_expr()
   if not good then
@@ -323,7 +343,7 @@ end
 -- comp_expr   →   “!” comp_expr
 --             |   arith_expr { ( “==” | “!=” | “<” | “<=” | “>” | “>=” ) arith_expr }
 function parse_comp_expr()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   if matchString("!") then
     good,ast = parse_comp_expr()
@@ -360,11 +380,11 @@ end
 -- parse_arith_expr
 -- arith_expr → term { ( “+” | “-” ) term }
 function parse_arith_expr()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   good,ast = parse_term()
   if not good then
-    return nil,false
+    return false,nil
   end
 
   while true do
@@ -386,11 +406,11 @@ end
 -- parse_term
 -- term → factor { ( “*” | “/” | “%” ) factor }
 function parse_term()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   good,ast = parse_factor()
   if not good then
-    return nil,false
+    return false,nil
   end
 
   while true do
@@ -417,7 +437,7 @@ end
 --        |   ( “true” | “false” )
 --        |   lvalue
 function parse_factor()
-  local oldlexstr, good, ast
+  local oldlexstr, good, ast, newast
 
   oldlexstr = lexstr
   if matchString("+") or matchString("-") then
@@ -425,7 +445,7 @@ function parse_factor()
     if not good then
       return false,nil
     end
-    ast = { {BIN_OP, oldlexstr}, ast, newast }
+    ast = { {UN_OP, oldlexstr}, newast }
 
   elseif matchString("(") then
     good,ast = parse_expr()
@@ -452,27 +472,22 @@ end
 -- parse_lvalue
 -- lvalue → VARID [ “[” expr “]” ]
 function parse_lvalue()
-  local oldlexstr, good, ast
-
+  local oldlexstr, good, ast, newast
   oldlexstr = lexstr
+  if matchCat(lexit.VARID) then
+    ast = { VARID_VAL, oldlexstr }
+  else
+    return false,nil
+  end
+
   if matchString("[") then
     good,newast = parse_expr()
     if not good or not matchString("]") then
       return false,nil
     end
     ast = { ARRAY_REF, { VARID_VAL, oldlexstr }, newast }
-
-  elseif matchCat(lexit.VARID) then
-    ast = { VARID_VAL, oldlexstr }
-
-  else
-    return false,nil
   end
-
   return true,ast
 end
-
-
-
 
 return parseit
